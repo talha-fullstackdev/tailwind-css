e476e8692da83b307b23a562bd8404a484ec7489
talha-fullstackdev <talhafullstack786@gmail.com>
2025-06-20 16:49:46 +0500

    changes width's to max width's in feedback component

--- src/assets/components/FeedBack.jsx
+++ src/assets/components/FeedBack.jsx
@@ -41,24 +41,24 @@ const FeedBack = () => {
 
   return (
     <div className="pt-[83px] pb-[84px] bg-[linear-gradient(180deg,_#F3F8FF_0%,_#E7EFFA_100%)] ">
-      <div className="w-[840px]  m-auto">
-        <div className="w-[739.18px]">
+      <div className="max-w-[840px]  m-auto">
+        <div className="max-w-[739.18px]">
         <p className="p-4 text-[36px] leading-[46px] tracking-[-0.72px] text-[#02033B] font-extrabold text-center inter">Get your freedom back, stop mobile spyware today</p>
         </div>
     
-      <div className="mt-[71.02px] w-[840px] bg-[#FFFFFF] pt-[72px] pr-[72px] pl-[72px] rounded-[48px]">
-        <div className="w-[696px] flex flex-wrap gap-[48px]">
+      <div className="mt-[71.02px] max-w-[840px] bg-[#FFFFFF] pt-[72px] pr-[72px] pl-[72px] rounded-[48px]">
+        <div className="max-w-[696px] flex flex-wrap gap-[48px]">
           {data.map((item, index) => (
-            <div key={index} className="w-[200] flex flex-col items-center justify-center ">
-              <img className="w-[77px]" src={item.image} alt="" />
-              <h2 className="w-[184px] inter text-center mt-[24px] text-[19px] font-bold leading-[25px] tracking-[-0.4px] text-[#02033B]">{item.heading}</h2>
-              <p className={`${index==0 ? "w-[196.48px]":""} ${index==1 ? "w-[184.54px]":""} ${index==2 ? "w-[197.62px]":""} ${index==3 ? "w-[193.15px]":""} ${index==4 ? "w-[198.48px]":""} ${index==5 ? "w-[198.22px]":""} mt-[24px] pl-[12.86px] pr-[12.62px] text-[16px] leading-[24px] tracking-[0%] inter text-center text-[#02033B] `}>{item.text}</p>
+            <div key={index} className="max-w-[200] flex flex-col items-center justify-center ">
+              <img className="max-w-[77px]" src={item.image} alt="" />
+              <h2 className="max-w-[184px] inter text-center mt-[24px] text-[19px] font-bold leading-[25px] tracking-[-0.4px] text-[#02033B]">{item.heading}</h2>
+              <p className={`${index==0 ? "max-w-[196.48px]":""} ${index==1 ? "max-w-[184.54px]":""} ${index==2 ? "max-w-[197.62px]":""} ${index==3 ? "max-w-[193.15px]":""} ${index==4 ? "max-w-[198.48px]":""} ${index==5 ? "max-w-[198.22px]":""} mt-[24px] pl-[12.86px] pr-[12.62px] text-[16px] leading-[24px] tracking-[0%] inter text-center text-[#02033B] `}>{item.text}</p>
             </div>
           ))}
         </div>
-        <div className="mt-[72px] w-[564.25px] pb-[48px] m-auto">
-          <button className="w-[268.66px] mb-[24px] bg-[#FFC247] text-[19px] leading-[22.5px] tracking-[0%] pl-[22px] pt-[16px] pr-[54.66px] pb-[15.5px] rounded-[42px] font-extrabold inter">Get Certo for iPhone</button>
-          <button className="w-[247.59px]  mb-[24px] border-[1px] text-[19px] border-[#02033B] rounded-[42px] ml-[24px] mr-[24px] font-extrabold inter leading-[22.5px] tracking-[0%]  pl-[22px] pt-[16px] pr-[23.59px] pb-[15.5px] ">Get Certo for Android</button>
+        <div className="mt-[72px] max-w-[564.25px] pb-[48px] m-auto">
+          <button className="max-w-[268.66px] mb-[24px] bg-[#FFC247] text-[19px] leading-[22.5px] tracking-[0%] pl-[22px] pt-[16px] pr-[54.66px] pb-[15.5px] rounded-[42px] font-extrabold inter">Get Certo for iPhone</button>
+          <button className="max-w-[247.59px]  mb-[24px] border-[1px] text-[19px] border-[#02033B] rounded-[42px] ml-[24px] mr-[24px] font-extrabold inter leading-[22.5px] tracking-[0%]  pl-[22px] pt-[16px] pr-[23.59px] pb-[15.5px] ">Get Certo for Android</button>
         </div>
       </div>
         </div>

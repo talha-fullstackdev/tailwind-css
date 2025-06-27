52320b3464d4fefbaf5786471680e85da5bea371
talha-fullstackdev <talhafullstack786@gmail.com>
2025-06-27 16:45:56 +0500

    made insight section and styles it

--- src/assets/components/Insights.jsx
+++ src/assets/components/Insights.jsx
@@ -15,7 +15,6 @@ const Insights = () => {
       heading: "How to Remove a Hacker from Your Samsung Phone",
       para: "Samsung is the second most popular manufacturer of smartphones in the world, with a market share of 28.19% compared to Apple’s 28.43% as...",
 
-
       buttonText: "Guides",
       buttonWidth: "94.27px",
     },
@@ -28,7 +27,6 @@ const Insights = () => {
     },
   ];
   return (
-
     <div className="bg-[#F3F8FF] pt-[84px] pb-[84px]">
       <div className="w-[1056px]  mb-[84px] m-auto">
         <div className="">
@@ -38,22 +36,47 @@ const Insights = () => {
         </div>
         <div className="mt-[72px] flex gap-[48px] ">
           {data.map((item, index) => (
-            <div key={index} className="w-[320px]  bg-[#FFFFFF] rounded-[48px] relative ">
-              <img className="rounded-tl-[48px] rounded-tr-[48px]" src={item.image} alt="" />
-              <p className={`mt-[47px] ${index==0&& "w-[177.89px]"} ${index==1&& "w-[198.18px]"} ${index==2&& "w-[205.81px]"}  ml-[48px] mr-[94.11px] ${index==2&& "text-[25px]"}   text-[#02033B] text-[23px] leading-[30px] tracking-[0%] inter font-bold`}>{item.heading}</p>
-              <p className="mt-[24px] text-[16px] leading-[24px] text-[#02033B] tracking-[0%] w-[221.9px] m-auto">{item.para}</p>
-              <button className={`${index==0 && "w-[112.41px]"} ${index==1 && "w-[94.27px]"} ${index==2 && "w-[112.41px]"} pt-[15px] pl-[20px] pb-[18px] pr-[20.41px] text-[15px] text-[#02033B] absolute bg-[#F3F8FF] rounded-[48px] top-[24px] left-[24px] leading-[20px] tracking-[0%] inter font-extrabold`} style={{ width: item.buttonWidth }}>
+            <div
+              key={index}
+              className="w-[320px]  bg-[#FFFFFF] rounded-[48px] relative "
+            >
+              <img
+                className="rounded-tl-[48px] rounded-tr-[48px]"
+                src={item.image}
+                alt=""
+              />
+              <p
+                className={`mt-[47px] ${index == 0 && "w-[177.89px]"} ${
+                  index == 1 && "w-[198.18px]"
+                } ${index == 2 && "w-[205.81px]"}  ml-[48px] mr-[94.11px] ${
+                  index == 2 && "text-[25px]"
+                }   text-[#02033B] text-[23px] leading-[30px] tracking-[0%] inter font-bold`}
+              >
+                {item.heading}
+              </p>
+              <p className="mt-[24px] text-[16px] leading-[24px] text-[#02033B] tracking-[0%] w-[221.9px] m-auto">
+                {item.para}
+              </p>
+              <button
+                className={`${index == 0 && "w-[112.41px]"} ${
+                  index == 1 && "w-[94.27px]"
+                } ${
+                  index == 2 && "w-[112.41px]"
+                } pt-[15px] pl-[20px] pb-[18px] pr-[20.41px] text-[15px] text-[#02033B] absolute bg-[#F3F8FF] rounded-[48px] top-[24px] left-[24px] leading-[20px] tracking-[0%] inter font-extrabold`}
+                style={{ width: item.buttonWidth }}
+              >
                 {item.buttonText}
               </button>
             </div>
           ))}
         </div>
         <div className="mt-[72px] w-[226.06px] m-auto">
-        <button className="pl-[20px] pt-[16px] pr-[51.06px] pb-[15.5px] bg-[#FFC247] rounded-[42px] text-[19px] leading-[22.5px]tracking-[0%] inter font-extrabold">View all insights</button>
+          <button className="pl-[20px] pt-[16px] pr-[51.06px] pb-[15.5px] bg-[#FFC247] rounded-[42px] text-[19px] leading-[22.5px]tracking-[0%] inter font-extrabold">
+            View all insights
+          </button>
         </div>
       </div>
     </div>
-  
   );
 };
 
